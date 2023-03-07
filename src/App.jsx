a03c2426c259cbfa552e5d9665bafc46802ca122
nesuesu <viktor.lavrinovic@gmail.com>
2023-03-07 12:55:10 +0200

    initial setting up

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header>Logo / Menu / User</header>
+      <body>
+        <h1>Home</h1>
+      </body>
+      <footer>(c) 2023 All rights reserved</footer>
+    </>
+  );
+}
+
+export default App;
