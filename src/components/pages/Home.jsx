86ffdfae5a8624b9b65d589c283ff85c47856373
nesuesu <viktor.lavrinovic@gmail.com>
2023-03-09 20:44:05 +0200

    Flowers on the Hope page from a data file

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,6 +1,9 @@
 import { useContext } from "react";
 import UserContext from "../../contexts/UserContext";
 
+import data from '../../data/flowers.json'
+
+
 const Home = () => {
 
     const {loggedInUser} = useContext(UserContext);
@@ -12,7 +15,15 @@ const Home = () => {
             <img style={{opacity:'0.25', display:'block', margin:'auto'}} src="https://www.deardrmooney.com/wp-content/uploads/2019/05/Depositphotos_15193351_l-2015-1038x564.jpg" alt="welcome" />
             </>
         :
-            <h1>Welcome</h1>
+        <div className="flowers">
+            {data.flowers.map(flower =>
+                <div>
+                    <h3 style={{textAlign:'center', color:'black'}}>{flower.name}</h3>
+                    <img src={flower.image} alt="flower" />
+                    <p style={{textAlign:'center'}}>color: {flower.color}</p>
+                </div>
+            )}
+        </div>
         }
         </>       
     )
